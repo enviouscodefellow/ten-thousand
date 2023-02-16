3f153605c9236d0ea420913e9613d9fbc30323ef
Nick <enviouscodefellow@outlook.com>
2023-02-16 15:43:56 -0700

    lab 44 complete

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,87 +1,104 @@
-from random import randint
 from collections import Counter
+from random import randint
+
 
 class GameLogic:
-    def roll_dice(dice):
-        roll = []
-        for die in range(0, dice):
-            roll.append(randint(1, 6))
-            # print(roll)
-        return tuple(sorted(roll))
+    @staticmethod
+    def roll_dice(num=6):
+        # version_1
 
+        return tuple([randint(1, 6) for _ in range(num)])
 
+    @staticmethod
     def calculate_score(dice):
-        score_dice = 0
-        # dice_counter = Counter(dice)
-        # dice_items = dice_total.items()
-        dice_scoring_tupl = tuple(
-            [
-                (tuple(), 0),
-                ((1,), 100),
-                ((1, 1), 200),
-                ((1, 1, 1), 1000),
-                ((1, 1, 1, 1), 2000),
-                ((1, 1, 1, 1, 1), 3000),
-                ((1, 1, 1, 1, 1, 1), 4000),
-                ((2,), 0),
-                ((2, 2), 0),
-                ((2, 2, 2), 200),
-                ((2, 2, 2, 2), 400),
-                ((2, 2, 2, 2, 2), 600),
-                ((2, 2, 2, 2, 2, 2), 800),
-                ((3,), 0),
-                ((3, 3), 0),
-                ((3, 3, 3), 300),
-                ((3, 3, 3, 3), 600),
-                ((3, 3, 3, 3, 3), 900),
-                ((3, 3, 3, 3, 3, 3), 1200),
-                ((4,), 0),
-                ((4, 4), 0),
-                ((4, 4, 4), 400),
-                ((4, 4, 4, 4), 800),
-                ((4, 4, 4, 4, 4), 1200),
-                ((4, 4, 4, 4, 4, 4), 1600),
-                ((5,), 50),
-                ((5, 5), 100),
-                ((5, 5, 5), 500),
-                ((5, 5, 5, 5), 1000),
-                ((5, 5, 5, 5, 5), 1500),
-                ((5, 5, 5, 5, 5, 5), 2000),
-                ((6,), 0),
-                ((6, 6), 0),
-                ((6, 6, 6), 600),
-                ((6, 6, 6, 6), 1200),
-                ((6, 6, 6, 6, 6), 1800),
-                ((6, 6, 6, 6, 6, 6), 2400),
-                ((1, 2, 3, 4, 5, 6), 1500),
-                ((2, 2, 3, 3, 4, 6), 0),
-                ((2, 2, 3, 3, 6, 6), 1500),
-                ((1, 1, 1, 2, 2, 2), 1200),
-            ],
-        )
-        dice_scoring_list = list(dice_scoring_tupl)
+        """
+        dice is a tuple of integers that represent the user's selected dice pulled out from current roll
+        """
+        # version_1
 
-        dice_scoring_dict = dict((key, value) for key, value in dice_scoring_tupl)
-        dice_scoring_counter_list = []
-        for key in dice_scoring_dict:
-            dice_scoring_counter_list.append(Counter(key))
-        # print(dice_scoring_counter_list)
-        dice_scoring_dict_values = dice_scoring_dict.values()
-        # print(list(dice_scoring_dict_values))
-        counter_scoring_tupl = tuple(zip(dice_scoring_counter_list, dice_scoring_dict_values))
-        # print(counter_scoring_tupl)
-        # counter_scoring_dict = dict((key, value) for key, value in counter_scoring_tupl)
+        if len(dice) > 6:
+            raise Exception("Cheating Cheater!")
 
-        # print(dice)
-        score_dice = dice_scoring_dict.get(dice)
-        return score_dice
+        counts = Counter(dice)
 
-        # if not dice:
-        #     return 0
-        # elif dice:
-        #     score_dice = counter_scoring_dict.get(dice_counter)
+        if len(counts) == 6:
+            return 1500
 
+        if len(counts) == 3 and all(val == 2 for val in counts.values()):
+            return 1500
 
-# print(GameLogic.roll_dice(6))
-# print(GameLogic.calculate_score(GameLogic.roll_dice(6)))
+        score = 0
 
+        ones_used = fives_used = False
+
+        for num in range(1, 6 + 1):
+
+            pip_count = counts[num]
+
+            if pip_count >= 3:
+
+                if num == 1:
+
+                    ones_used = True
+
+                elif num == 5:
+
+                    fives_used = True
+
+                score += num * 100
+
+                # handle 4,5,6 of a kind
+                pips_beyond_3 = pip_count - 3
+
+                score += score * pips_beyond_3
+
+                # bug if 2 threesomes? Let's test it
+
+                # 1s are worth 10x
+                if num == 1:
+                    score *= 10
+
+        if not ones_used:
+            score += counts.get(1, 0) * 100
+
+        if not fives_used:
+            score += counts.get(5, 0) * 50
+
+        return score
+
+    @staticmethod
+    def validate_keepers(roll, keepers):
+        # version_3
+
+        # pro tip: you can do some math operations with counters
+        # check https://docs.python.org/3/library/collections.html#collections.Counter
+        keeper_counter = Counter(keepers)
+        roll_counter = Counter(roll)
+
+        # a "valid" result is an empty Counter result
+        result = keeper_counter - roll_counter
+
+        # an empty Counter is falsy, so use "not" to flip it
+        return not result
+
+    @staticmethod
+    def get_scorers(dice):
+        # version_3
+
+        all_dice_score = GameLogic.calculate_score(dice)
+
+        if all_dice_score == 0:
+            return tuple()
+
+        scorers = []
+
+        # for i in range(len(dice)):
+
+        for i, val in enumerate(dice):
+            sub_roll = dice[:i] + dice[i + 1 :]
+            sub_score = GameLogic.calculate_score(sub_roll)
+
+            if sub_score != all_dice_score:
+                scorers.append(val)
+
+        return tuple(scorers)
